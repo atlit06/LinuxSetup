30bfe1f7cbf0396b35f6c3d6fb28c8cc8973bc44
Hrafn Orri Hrafnkelsson <HrafnOrri1207@gmail.com>
2017-11-20 19:30:30 +0000

    update

--- ubuntu/desktop/xenial/dotnetcore/install.sh
+++ ubuntu/desktop/xenial/dotnetcore/install.sh
@@ -4,6 +4,6 @@ set -e
 
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-get update
-sudo apt-get -y install dotnet-sdk-2.0.2
+sudo apt-get -y --allow-unauthenticated install dotnet-sdk-2.0.2
 
 exit 0
