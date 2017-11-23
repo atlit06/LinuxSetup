245facde8da07435e0c21fa2daa2a0e805e2d398
Hrafn Orri Hrafnkelsson <HrafnOrri1207@gmail.com>
2017-11-23 15:56:15 +0000

    upgraded to node 8

--- ubuntu/desktop/xenial/nodejs/install.sh
+++ ubuntu/desktop/xenial/nodejs/install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get -y install build-essential
 sudo apt-get -y install nodejs
 
